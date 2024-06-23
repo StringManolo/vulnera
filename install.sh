6fcb3ecafa38739eadac15495c8ec5c1edfcda50
StringManolo <manuelvarelacaldas@gmail.com>
2024-06-23 07:15:55 +0000

    Add debug text to install.sh

--- install.sh
+++ install.sh
@@ -18,19 +18,27 @@ fi
 echo 'Installing servers node modules...'
 npm install
 
+echo '\n\nInstalling vulnera...-
+
 # install vulnera
 SCRIPT_DIR=$(pwd)
 if [ -d "/data/data/com.termux" ]; then
   echo 'Termux detected, installing in termux'
   cp ./vulnera.sh /data/data/com.termux/usr/bin/vulnera
+  echo 'Bin created at /data/data/com.termux/usr/bin/vulnera'
   chmod 775 /data/data/com.termux/usr/bin/vulnera
+  echo 'Permissions set'
   ln -s "$SCRIPT_DIR" "/data/data/com.termux/usr/bin/.vulnera_folder"
+  echo "Link to $SCRIPT_DIR created at /data/data/com.termux/usr/bin/.vulnera_folder"
   echo "Installed in termux. You can run $ vulnera help"
 else
   echo 'Linux detected, installing in linux'
   cp vulnera.sh /bin/vulnera
+  echo 'Bin created at /bin/vulnera'
   chmod 775 /bin/vulnera
+  echo 'Permissions set'
   ln -s "$SCRIPT_DIR" "/usr/bin/.vulnera_folder"
+  echo "Link to $SCRIPT_DIR created at /usr/bin/.vulnera_folder"
   echo "Installed in linux. You can run $ vulnera help"
 fi 
 
