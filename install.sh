659a3e21af41836d48960e54b8fbb98cfe2a9a4e
StringManolo <manuelvarelacaldas@gmail.com>
2024-06-23 07:08:32 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,37 @@
 #!/usr/bin/env bash
 
+# Check if node.js is installed
+if ! command -v node &> /dev/null
+then
+    echo "Node.js is not installed. Install it and try again"
+    exit
+fi
+
+# Check if npm is installed
+if ! command -v npm &> /dev/null
+then
+    echo "NPM is not installed. Install it and try again"
+    exit
+fi
+
 # install node modules
+echo 'Installing servers node modules...'
 npm install
 
+# install vulnera
 SCRIPT_DIR=$(pwd)
-
 if [ -d "/data/data/com.termux" ]; then
+  echo 'Termux detected, installing in termux'
   cp ./vulnera.sh /data/data/com.termux/usr/bin/vulnera
   chmod 775 /data/data/com.termux/usr/bin/vulnera
   ln -s "$SCRIPT_DIR" "/data/data/com.termux/usr/bin/.vulnera_folder"
-  echo "Installed in termux"
+  echo "Installed in termux. You can run $ vulnera help"
 else
+  echo 'Linux detected, installing in linux'
   cp vulnera.sh /bin/vulnera
   chmod 775 /bin/vulnera
   ln -s "$SCRIPT_DIR" "/usr/bin/.vulnera_folder"
-  echo "Installed in linux"
+  echo "Installed in linux. You can run $ vulnera help"
 fi 
 
+echo -e 'WARNING:\nDO NOT DELETE OR MOVE THIS FOLDER. IT WILL BE USED BY VULNERA TO RUN SERVERS, LOCATE MODULES, AND OTHER FILES.'
